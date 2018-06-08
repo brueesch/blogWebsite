e4d48115488c76a88b448164e8b2d06c1d1bc218
Christian Brüesch <brueesch2@gmail.com>
2018-06-08 22:34:44 +0200

    Fix Error

--- src/app/views/travel-blog/travel-blog.component.ts
+++ src/app/views/travel-blog/travel-blog.component.ts
@@ -1,9 +1,9 @@
-import { Component, HostBinding, OnInit, OnDestroy } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   templateUrl: './travel-blog.component.html'
 })
-export class TravelBlogComponent implements OnInit, OnDestroy {
+export class TravelBlogComponent {
   @HostBinding('class.bw-travel-blog') theme = true;
 
 }
