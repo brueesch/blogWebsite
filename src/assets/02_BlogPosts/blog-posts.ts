42a79cab150ad2f9447a5fa8059ecfa3389a80c5
Christian Brüesch <brueesch2@gmail.com>
2018-07-17 07:23:47 +0200

    Added new Post

--- src/assets/02_BlogPosts/blog-posts.ts
+++ src/assets/02_BlogPosts/blog-posts.ts
@@ -10,7 +10,7 @@ export let blogPosts = {
     subtitle3: 'Über den Mont Tendre',
     location: 'Mont Tendre | Kanton Waadt | Schweiz',
     intro: 'Ich begann meine Reise zum Mont Tendre in Le Brassus, indem ich für ' +
-            'etwa 15 Minuten nach Les Bioux fuhr, wo ich mein Auto parkte und die Wanderung begann....',
+            'etwa 15 Minuten nach Les Bioux fuhr, wo ich mein Auto parkte und die Wanderung begann...',
     text1: 'Ich begann meine Reise zum Mont Tendre in Le Brassus, indem ich für etwa ' +
             '15 Minuten nach Les Bioux fuhr, wo ich mein Auto parkte und die Wanderung begann.',
     text2: 'Ich brauchte etwa 2 Stunden, um den Gipfel zu erreichen. Nach einem kurzen Teil ' +
@@ -32,5 +32,43 @@ export let blogPosts = {
             'nordwestlich von Morges. Mit 1679 m ü. M. ist der Mont Tendre die höchste Erhebung im ' +
             'Schweizer Jura sowie eine der höchsten im gesamten Juragebirge. Einzig die Gipfel der ' +
             'Crêt de la Neige-Kette in Frankreich überragen ihn noch um etwa 40 Meter.'
+  },
+  2: {
+    id: 2,
+    storyTitle: 'Guten Morgen Chasseral',
+    image1: 'assets/01_BlogEntries/02_Chasseral/Chasseral3_small-5374.jpg',
+    image2: 'assets/01_BlogEntries/02_Chasseral/Chasseral1_small-5339.jpg',
+    image3: 'assets/01_BlogEntries/02_Chasseral/Chasseral2_small-5370.jpg',
+    subtitle1: 'Auf zum Gipfel',
+    //subtitle2: 'Ausser Atem',
+    subtitle2: 'Angenehme Überraschung',
+    subtitle3: 'Über den Chasseral',
+    location: 'Chasseral | Kanton ? | Schweiz',
+    intro: 'Es war gerade mal 4:30 als mein Alarm im Hotelzimmer in Biel ertönte. Heutiges Ziel: Der Chasseral.',
+    text1: 'Es war gerade mal 4:30 als mein Alarm im Hotelzimmer in Biel ertönte. Heutiges Ziel: Der Chasseral. ' +
+            'Ich fuhr von Biel aus los. Mit dem Auto fährt man ungefähr 45 Minuten bis zum Hotel Chasseral. Wie üblich ' +
+            'habe ich mich auf den ganzen Trip nicht ausgiebig vorbereitet und war knapp in der Zeit. Als ich auf dem ' +
+            'grossen öffentlichen Parkplatz beim Hotel ankam, blieben mir nur noch 20 Minuten bis zum Sonnenaufgang. ' +
+            'Vom Parkplatz aus führt eine betonierte Strasse bis zum Gipfel, welche zu Fuss begangen werden muss. Gemäss ' +
+            'Google Maps dauert die Strecke zu Fuss ungefähr 20 Minuten, die mir jedoch nicht mehr zur Verfügung standen.',
+    text2: 'Ich kam etwa 5 Minuten vor dem Sonnenaufgang auf dem Gipfel an, völlig ausser Atem, am Schwitzen und hektisch ' +
+            'die Umgebung erforschend um ein gutes Motiv zu finden. Sport war auf alle Fälle für den Tag schon abgehakt und ' +
+            'es war noch nicht mal 6 Uhr morgens. Mein Ziel für das Motiv war ein wenig anders als meine bisherigen Aufnahmen. ' +
+            'Da die Sonne vielfach ein fester Bestandteil meiner Fotos ist, wollte ich dieses mal etwas neues ausprobieren und ' +
+            'die Umgebung fotografieren ohne die Sonne im Bild zu haben. Ich hatte nur einige Minuten übrig um abzuschätzen wo ' +
+            'genau die Sonne aufgehen wird und dementsprechend mein Stativ auszurichten.',
+    text3: 'Ich ass mein Frühstück und genoss die Landschaft, während meine Kamera vor sich hin knipste und die Sonne langsam ' +
+            'aufging. Es war sehr kühl auf dem Gipfel und es wäre sicherlich eine gute Idee gewesen meinen Pullover nicht im Hotel ' +
+            'zu vergessen, ich liess mir jedoch meine gute Laune nicht von der kühlen Brise verderben.',
+    text4:  'Kurz vor dem Sonnenaufgang hatten sich Schleierwolken gebildet und die Region wurde mit einem leichten Nebel eingedeckt. Dadurch wurde zwar die ' +
+            'Sichtweite beeinträchtigt, jedoch ergab sich durch den Nebel eine mystische sowie auch sehr angenehme Stimmung, die ' +
+            'sich auch in den Fotos widerspiegelt. So wurde ich, als ich auf dem Gipfel herumlief, von einer sensationellen Szenerie ' +
+            'überrascht. Es war das erste mal auf einem meiner Ausflüge, wo das Bild vom Sonnenaufgang nicht das Beste war, sondern ' +
+            'eines welches ich fast eine Stunde später schoss.',
+    text5: 'Der Chasseral scheint mir ein super Ausflugsziel zu sein. Auf dem Heimweg sind mir einige Fahrrad- und Motorradfahrer entgegen ' +
+            'gekommen und ich bin mir sicher, dass der Chasseral an diesem Tag von dutzenden Wanderern besucht wurde. Durch die gute Erschliessung ' +
+            'kann der Berg mit dem Auto oder auch zu Fuss erklommen werden. Daher ein perfektes Ausflugsziel für Wanderer, aber auch Familien welche ' +
+            'nicht stundenlang einen Berg besteigen möchten. Für mich war mein erstes kleines Abenteuer bereits beendet, als ich um 8 Uhr ins Bett ' +
+            'fiel, um nochmals 2 Stunden Kraft zu tanken bevor ich den restlichen Tag in Angriff nahm.'
   }
 };
